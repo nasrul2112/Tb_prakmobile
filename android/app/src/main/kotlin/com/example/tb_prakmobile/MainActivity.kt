693346847fe1023c7b5fc99d3792f6cdebaf6476
Anas  Nasrulloh <2006065@itg.ac.id>
2023-07-05 22:48:44 +0700

    Tugas Besar

---
+++ android/app/src/main/kotlin/com/example/tb_prakmobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tb_prakmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Models/database.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: unused_import
+
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+// These imports are used to open the database
+import 'package:drift/native.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'package:tb_prakmobile/Models/transaction_with_Category.dart';
+import 'package:tb_prakmobile/models/category.dart';
+import 'package:tb_prakmobile/models/transaction.dart';
+
+part 'database.g.dart';
+
+@DriftDatabase(
+    // relative import for the drift file. Drift also supports `package:`
+    // imports
+    tables: [Categories, Transactions])
+class AppDb extends _$AppDb {
+  AppDb() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  Future updateCategoryRepo(int id, String newName) async {
+    return (update(categories)..where((t) => t.id.equals(id))).write(
+      CategoriesCompanion(
+        name: Value(newName),
+      ),
+    );
+  }
+
+  Future deleteCategoryRepo(int id) async {
+    return (delete(categories)..where((t) => t.id.equals(id))).go();
+  }
+
+  Future<List<Category>> getAllCategoryRepo(int type) async {
+    return await (select(categories)..where((tbl) => tbl.type.equals(type)))
+        .get();
+  }
+
+  Stream<List<TransactionWithCategory>> getTransactionByDateRepo(
+      DateTime date) {
+    final query = (select(transactions).join([
+      innerJoin(categories, categories.id.equalsExp(transactions.category_id))
+    ])
+      ..where(transactions.transaction_date.equals(date)));
+    return query.watch().map((rows) {
+      return rows.map((row) {
+        return TransactionWithCategory(
+          row.readTable(transactions),
+          row.readTable(categories),
+        );
+      }).toList();
+    });
+  }
+
+  Future updateTransactionRepo(int id, int amount, int categoryId,
+      DateTime transactionDate, String nameDetail) async {
+    return (update(transactions)..where((t) => t.id.equals(id))).write(
+      TransactionsCompanion(
+          name: Value(nameDetail),
+          amount: Value(amount),
+          category_id: Value(categoryId),
+          transaction_date: Value(transactionDate)),
+    );
+  }
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+
+    return NativeDatabase.createInBackground(file);
+  });
+}
